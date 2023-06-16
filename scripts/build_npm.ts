a6a448c7e41b3ca5d6d253a226606e61e0c4f19d
NeKz <nekzor@users.noreply.github.com>
2023-06-16 18:58:12 +0200

    Optimize npm package

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -17,11 +17,14 @@ const outDir = './npm';
 
 await emptyDir(outDir);
 
+// These files are used for unit tests.
 await copy('./demos/public', 'npm/esm/demos/public', { overwrite: true });
 await copy('./demos/public', 'npm/script/demos/public', { overwrite: true });
 
 await build({
     entryPoints: ['./src/mod.ts'],
+    testPattern: '**.ts',
+    rootTestDir: './tests',
     outDir,
     shims: {
         deno: true,
@@ -29,7 +32,7 @@ await build({
     package: {
         name: '@nekz/sdp',
         version,
-        description: 'Source Engine demo parser.',
+        description: 'Simple Source Engine demo parser.',
         keywords: ['Source Engine'],
         homepage: 'https://sdp.nekz.me',
         bugs: {
@@ -48,8 +51,33 @@ await build({
     },
 });
 
+// Remove unnecessary fields.
+
+const npmPackage = JSON.parse(await Deno.readTextFile('./npm/package.json'));
+
+delete npmPackage.scripts;
+delete npmPackage.devDependencies;
+
+await Deno.writeTextFile('./npm/package.json', JSON.stringify(npmPackage, null, 2));
+
+// Ignore files which should not get published.
+
 await Deno.writeTextFile(
     'npm/.npmignore',
-    'esm/demos/public/\nscript/demos/public/\n',
+    [
+        // Deps, shims and tests are only used for unit testing.
+        // There is no need to keep them in the package.
+        'esm/deps/',
+        'esm/tests/',
+        'esm/_dnt.test_shims.d.ts',
+        'esm/_dnt.test_shims.js',
+        'script/deps/',
+        'script/tests/',
+        'script/_dnt.test_shims.d.ts',
+        'script/_dnt.test_shims.js',
+        // Also ignore the copied test data.
+        'esm/demos/public/',
+        'script/demos/public/',
+    ].join('\n') + '\n',
     { append: true },
 );

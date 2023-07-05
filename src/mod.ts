6968b7d14968bee636f5b689859e740666ec24b2
NeKz <nekzor@users.noreply.github.com>
2023-07-05 15:26:56 +0200

    Fix exports

--- src/mod.ts
+++ src/mod.ts
@@ -4,16 +4,17 @@
  * SPDX-License-Identifier: MIT
  */
 
-export { SourceDemo } from './demo.ts';
+export * from './demo.ts';
+export * as Messages from './messages.ts';
 export { DemoMessages } from './messages.ts';
-export { SourceDemoParser } from './parser.ts';
+export * from './parser.ts';
 export * as DataTables from './types/DataTables.ts';
-export { GameEventManager } from './types/GameEventManager.ts';
-export { NetMessages } from './types/NetMessages.ts';
-export { SoundInfo } from './types/SoundInfo.ts';
+export * from './types/GameEventManager.ts';
+export * as NetMessages from './types/NetMessages.ts';
+export * from './types/SoundInfo.ts';
 export * as StringTables from './types/StringTables.ts';
 export { UserCmd } from './types/UserCmd.ts';
-export { SourceGames } from './speedrun/games/mod.ts';
-export type { SourceGame, SourceGameRule } from './speedrun/games/SourceGame.ts';
-export { SarReplay, SarTimer } from './speedrun/sar.ts';
-export { SourceTimer, TimingResult } from './speedrun/timer.ts';
+export * from './speedrun/games/mod.ts';
+export * from './speedrun/games/SourceGame.ts';
+export * from './speedrun/sar.ts';
+export * from './speedrun/timer.ts';

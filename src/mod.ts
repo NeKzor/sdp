3df3a668757c0758bf81ed7dc0ac9947fe3737c4
NeKz <nekzor@users.noreply.github.com>
2023-07-17 04:48:58 +0200

    Fix missing exports

--- src/mod.ts
+++ src/mod.ts
@@ -4,17 +4,22 @@
  * SPDX-License-Identifier: MIT
  */
 
-export * from './demo.ts';
-export * as Messages from './messages.ts';
-export { DemoMessages } from './messages.ts';
-export * from './parser.ts';
-export * as DataTables from './types/DataTables.ts';
-export * from './types/GameEventManager.ts';
-export * as NetMessages from './types/NetMessages.ts';
-export * from './types/SoundInfo.ts';
-export * as StringTables from './types/StringTables.ts';
-export { UserCmd } from './types/UserCmd.ts';
 export * from './speedrun/games/mod.ts';
 export * from './speedrun/games/SourceGame.ts';
 export * from './speedrun/sar.ts';
 export * from './speedrun/timer.ts';
+export * from './types/CmdInfo.ts';
+export * as DataTables from './types/DataTables.ts';
+export * from './types/GameEventManager.ts';
+export * as NetMessages from './types/NetMessages.ts';
+export * from './types/QAngle.ts';
+export * from './types/SoundInfo.ts';
+export * as StringTables from './types/StringTables.ts';
+export { UserCmd } from './types/UserCmd.ts';
+export * from './types/UserMessages.ts';
+export * from './types/Vector.ts';
+export * from './buffer.ts';
+export * from './demo.ts';
+export * as Messages from './messages.ts';
+export { DemoMessages } from './messages.ts';
+export * from './parser.ts';
--- src/speedrun/games/mod.ts
+++ src/speedrun/games/mod.ts
@@ -1,14 +1,21 @@
-import Portal from './Portal.ts';
 /*
  * Copyright (c) 2018-2023, NeKz
  *
  * SPDX-License-Identifier: MIT
  */
 
+import Portal from './Portal.ts';
 import Portal2 from './Portal2.ts';
 import PortalStoriesMel from './PortalStoriesMel.ts';
 import ApertureTag from './ApertureTag.ts';
 import { SourceGame } from './SourceGame.ts';
+
+export const Games = {
+    Portal,
+    Portal2,
+    PortalStoriesMel,
+    ApertureTag,
+};
 
 // prettier-ignore
 export const SourceGames: SourceGame[] = [

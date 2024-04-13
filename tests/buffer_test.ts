ba03d1fa9a2fc2397f8c745831a77ff2be9b848d
NeKz <nekzor@users.noreply.github.com>
2024-04-13 12:44:11 +0100

    New SourceBuffer class

--- tests/buffer_test.ts
+++ tests/buffer_test.ts
@@ -6,7 +6,7 @@
 
 import { assertEquals } from 'https://deno.land/std@0.191.0/testing/asserts.ts';
 import { describe, it } from 'https://deno.land/std@0.191.0/testing/bdd.ts';
-import { SourceDemoBuffer } from '../src/buffer.ts';
+import { SourceBuffer, SourceDemoBuffer } from '../src/buffer.ts';
 
 describe('SourceDemoBuffer', () => {
     it('write bits correctly', () => {
@@ -17,5 +17,17 @@ describe('SourceDemoBuffer', () => {
         const reader = new SourceDemoBuffer(writer.buffer);
         assertEquals(reader.readBits(1), 0);
         assertEquals(reader.readBits(11), 128);
+    });
+});
+
+describe('SourceBuffer', () => {
+    it('write bits correctly', () => {
+        const writer = SourceBuffer.allocate(100);
+        writer.writeBitsLE(0, 1);
+        writer.writeBitsLE(128, 11);
+
+        const reader = new SourceBuffer(writer.buffer);
+        assertEquals(reader.readBitsLE(1), 0);
+        assertEquals(reader.readBitsLE(11), 128);
     });
 });
